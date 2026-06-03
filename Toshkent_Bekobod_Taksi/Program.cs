bcb94d1a4258de0bed1956d1b4cad81b1da0bf75
Developr <golibjonabdurasulov47@gmail.com>
2026-06-03 12:56:42 +0500

    change

--- Toshkent_Bekobod_Taksi/Program.cs
+++ Toshkent_Bekobod_Taksi/Program.cs
@@ -12,6 +12,7 @@ var app = builder.Build();
 var bot = app.Services.GetRequiredService<BotService>();
 _ = bot.StartReceiving();
 
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Urls.Clear();
 app.Urls.Add($"http://0.0.0.0:{port}");
